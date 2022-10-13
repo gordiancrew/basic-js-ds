ac6f6778db4528147501687a26dce82205ca6341
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-10-13 23:59:57 +0300

    fix: solution stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.arr=new Array();
+  }
+  push(element ) {
+    this.arr.unshift(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // let res =this.arr[this.arr.length-1];
+    
+   return this.arr.shift();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
+    return this.arr[0];
   }
 }
 
