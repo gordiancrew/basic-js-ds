61c3ed63b5d1d2e9d9352a5be88120fdd37866fa
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-10-14 00:30:24 +0300

    fix: task complite

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.arr=new Array();
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+   return arrayToList(this.arr);
+   function arrayToList(array) {
+    var list = null;
+    for (var i = array.length - 1; i >= 0; i--)
+      list = {value: array[i], next: list};
+    return list;
+  }
+  }
+
+  enqueue( value ) {
+   this.arr.push(value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.shift();
   }
 }
 
